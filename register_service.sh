23e2a5477e1986b7b0e9cd83dde5f009323bc1c8
xeto888 <111055747+xeto888@users.noreply.github.com>
2022-08-11 13:30:47 +0200

    Update register_service.sh

--- register_service.sh
+++ register_service.sh
@@ -3,7 +3,7 @@
 #The service uses an incoming cert / pubkey for encrypting the client certs. After encryption, the service sends the certs back to the issuer. 
 
 sleep 5
-mosquitto_sub -h 127.0.0.1 -p 666 -t regist -u xeto888 -P <MYPW> | while read -r line
+mosquitto_sub -h 127.0.0.1 -p 666 -t <registertopic> -u <user> -P <MYPW> | while read -r line
 
 # For key creation the openssl is recommended: openssl req -x509 -nodes -newkey rsa:2048 -keyout private-key.pem -out public-key.pem
 
@@ -15,14 +15,14 @@ do
     cat decoded_payload > received_pubkey
     openssl smime -encrypt -binary -aes-256-cbc -in /etc/mosquitto/client-certificates -out encrypted_certs -outform DER received_pubkey
     b64_cert=$(base64 -w 0 encrypted_zip)
-    mosquitto_pub -h 127.0.0.1 -p 666 -t Information -u xeto888 -P <MYPW> -m "Registration Completed"
-    mosquitto_pub -h 127.0.0.1 -p 666 -t Information -u xeto888 -P <MYPW> -m $b64_cert
+    mosquitto_pub -h 127.0.0.1 -p 666 -t <informationtopic> -u <user> -P <MYPW> -m "Registration Completed"
+    mosquitto_pub -h 127.0.0.1 -p 666 -t <informationtopic> -u <user> -P <MYPW> -m $b64_cert
     rm decoded_payload
     rm received_pubkey
     rm encrypted_certs
 
   else 
-    mosquitto_pub -h 127.0.0.1 -p 666 -t Information -u xeto888 -P <MYPW> -m "Invalid Public Key"
+    mosquitto_pub -h 127.0.0.1 -p 666 -t <informationtopic> -u xeto888 -P <MYPW> -m "Invalid Public Key"
   fi
   echo "" > cmds
 done
